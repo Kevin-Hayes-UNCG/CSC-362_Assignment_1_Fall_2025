34f9039f5ca55ed9006daa705df3f7900cab1fbd
Kevin-Hayes-UNCG <89085983+Kevin-Hayes-UNCG@users.noreply.github.com>
2025-08-21 12:15:23 -0400

    Create question1.c

---
+++ question1.c
@@ -0,0 +1,8 @@
+//Place your needed include statements here (This is roughly the equivelent of libraries in python)
+
+
+int main(int argc, char* argv[]){
+  //Place your solution code here
+
+  return 0;
+}
